51810ca970c1e6e0f102fa73f09beb879ff9d182
Alex Manuel Gomez Salazar <168044669+Lic-AlexGomez@users.noreply.github.com>
2024-11-09 06:31:20 +0000

    navbar add logo y dropdown

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import logo from "../../img/logo.png";
 export const Navbar = () => {
+	
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0  h1">	<img  className="logo mx-lg-4" src={logo} /></span>
 			</Link>
 			<div className="ml-auto">
+			<div class="dropdown">
+					<button class="btn btn-primary dropdown-toggle px-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+						Favorites <span class="badge bg-secondary">4</span>
+					</button>
+					<ul class="dropdown-menu">
+						<li><a class="dropdown-item" href="#">Action</a></li>
+						<li><a class="dropdown-item" href="#">Another action</a></li>
+						<li><a class="dropdown-item" href="#">Something else here</a></li>
+					</ul>
+					</div>
 				<Link to="/demo">
 					<button className="btn btn-primary">Check the Context in action</button>
 				</Link>
